73fa513e7de1667423dbedc6148709ea21336025
Mustapha Abdullahi S <donsol007@gmail.com>
2025-11-14 09:34:00 +0100

    Credit not bug fix 2
    Credit not bug fix 2

--- erp2erpfiscal/erp2erpfiscal/doctype/havano_zimra_user/havano_zimra_user.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Donsol and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Havano Zimra User", {
-// 	refresh(frm) {
-
-// 	},
-// });
